624369ba7991cde27f1195ca6abf115be75b7057
pablo calvo <pjcalvov@gmail.com>
2019-10-09 10:18:15 +0200

    adding logging

--- api/app.py
+++ api/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, abort
 from flask_restful import Resource, Api
 from core import money
+import logging
+
+logging.basicConfig(filename='logging.log',level=logging.DEBUG)
 
 app = Flask(__name__)
 api = Api(app)
@@ -10,9 +13,10 @@ class MoneyApi(Resource):
         value = request.args.get('value', 1, type=float)     
         validatedparam = money.formatMoney(value)
         if validatedparam == "":
+            logging.error('server error, not able to parse the value')
             abort(400) 
-            return {'error': 'server error, not able to parse the value'},400
         # return the parsed Value
+        logging.info(f'Original value: {value} . Formatted value: {validatedparam}')
         return {'parsedValue': validatedparam},201
 
 class Main(Resource):
