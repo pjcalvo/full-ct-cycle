b80a78df0526f53f2e2b54c095ebf6d2c9847ca7
pablo <pjcalvov@gmail.com>
2020-05-13 22:11:29 -0600

    added unit tests
    mend

--- ci/scripts/integration-tests.sh
+++ ci/scripts/integration-tests.sh
@@ -3,11 +3,8 @@ python3 -m venv venv
 . venv/bin/activate
 pip install -r requirements.txt
 
-export FLASK_APP = "src/web/app.py"
-export FLASK_RUN_PORT = "5000"
 
-
-flask run
+FLASK_APP=src/web/app.py FLASK_RUN_PORT=5000 flask run
 
 sleep 3
 
